d6b3a0a0ee69d37adccc400ba7d3444323f1e040
Kayvan Vakili <kvakili813@gmail.com>
2018-07-18 21:02:23 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,4 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  create_table "movies", force: :cascade do |t|
-    t.string  "title"
-    t.integer "release_date"
-    t.string  "director"
-    t.string  "lead"
-    t.boolean "in_theaters"
-  end
-
 end
